92806d86dbc11ff4b601ce40237aabbc0497b56f
Grzegorz <prywatne.grzegorztabat@gmail.com>
2021-09-28 19:53:15 +0200

    Update

--- model/model_training.py
+++ model/model_training.py
@@ -1,14 +1,12 @@
-import json
-import nltk
-from nltk.stem import WordNetLemmatizer
-import string
 import random
+import string
+
+import nltk
 import numpy as np
 import tensorflow as tf
+from nltk.stem import WordNetLemmatizer
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.python.keras import metrics
-from tensorflow.python.keras.layers.core import Dense
 
 class TrainingModel:
     def __init__(self, words, classes, data_x, data_y):
@@ -19,29 +17,31 @@ class TrainingModel:
         self.lemmatizer = WordNetLemmatizer()
 
     def train(self):
+
         words = [self.lemmatizer.lemmatize(word.lower()) for word in self.words if word not in string.punctuation]
+
         training = []
-        out_empty = [0]* len(self.classes)
+        out_empty = [0] * len(self.classes)
 
         for idx, doc in enumerate(self.data_x):
             bow = []
             text = self.lemmatizer.lemmatize(doc.lower())
 
-        for word in words:
-            bow.append(1) if word in text else bow.append(0)
+            for word in words:
+                bow.append(1) if word in text else bow.append(0)
 
-        output_row = list(out_empty)
-        output_row[self.classes.index(self.data_y[idx])] = 1
+            output_row = list(out_empty)
+            output_row[self.classes.index(self.data_y[idx])] = 1
 
-        training.append([bow, output_row])
+            training.append([bow, output_row])
 
         random.shuffle(training)
         training = np.array(training, dtype=object)
 
-        train_x = np.array(list(training[:,0]))
-        train_y = np.array(list(training[:,1]))
+        train_x = np.array(list(training[:, 0]))
+        train_y = np.array(list(training[:, 1]))
 
-        input_shape = (len(train_x[0]), )
+        input_shape = (len(train_x[0]),)
         output_shape = len(train_y[0])
 
         model = Sequential()
@@ -52,13 +52,13 @@ class TrainingModel:
         model.add(Dense(output_shape, activation="softmax"))
         adam = tf.keras.optimizers.Adam(learning_rate=0.01, decay=1e-6)
         model.compile(loss='categorical_crossentropy',
-            optimizer=adam,
-            metrics=["accuracy"])
+                      optimizer=adam,
+                      metrics=["accuracy"])
 
-        model.fit(x=train_x, y=train_y, epochs=200, verbose=0)
+        model.fit(x=train_x, y=train_y, epochs=200, verbose=1)
 
         return model
-    
+
     def get_intent(self, model, command):
         bow = self.bag_of_words(command, self.words)
         result = model.predict(np.array([bow]))[0]
@@ -67,15 +67,12 @@ class TrainingModel:
         y_pred = [[idx, res] for idx, res in enumerate(result) if res > thresh]
         y_pred.sort(key=lambda x: x[1], reverse=True)
 
-        intents =[]
-
-        for pred in y_pred:
-            intents.append(self.classes[pred[0]])
-        return intents
+        intent = self.classes[y_pred[0][0]]
+        return intent
 
     def bag_of_words(self, command, words):
         tokens = self.clean_text(command)
-        bow = [0]* len(words)
+        bow = [0] * len(words)
 
         for token in tokens:
             for idx, word in enumerate(words):
@@ -90,9 +87,11 @@ class TrainingModel:
         return tokens
 
     @staticmethod
-    def get_response(intents, data):
-        tag = intents[0]
+    def get_response(tag, data):
         list_of_intents = data['intents']
         for intent in list_of_intents:
             if intent['tag'] == tag:
-                return random.choice(intent['response'])
+                if len(intent['response']) > 0:
+                    return random.choice(intent['response'])
+                else:
+                    return None
