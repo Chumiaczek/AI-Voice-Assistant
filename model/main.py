6220e64f8dc282a78a2151e31d647a5526cee12f
Grzegorz <prywatne.grzegorztabat@gmail.com>
2021-09-28 19:25:06 +0200

    Update

--- model/main.py
+++ model/main.py
@@ -1,132 +1,15 @@
-import json
-import nltk
-import ssl
-from nltk.stem import WordNetLemmatizer
-import string
-import random
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.python.keras import metrics
-from tensorflow.python.keras.layers.core import Dense
+import model
+from model.model_training import TrainingModel
 
+if __name__ == '__main__':
+    words = model.words
+    classes = model.classes
 
-# Uncomment when running on new PC / Mac
+    training_model = TrainingModel(words, classes, model.data_x, model.data_y)
+    trained_model = training_model.train()
 
-#try:
-    #_create_unverified_https_context = ssl._create_unverified_context
-#except AttributeError:
-    #pass
-#else:
-    #ssl._create_default_https_context = _create_unverified_https_context
-
-#nltk.download('punkt')
-#nltk.download('wordnet')
-
-with open('config/config.json') as file:
-    data = json.load(file)
-
-words = []
-classes = []
-data_x = []
-data_y = []
-
-for intent in data['intents']:
-    for utterance in intent['utterances']:
-        tokens = nltk.word_tokenize(utterance)
-        words.extend(tokens)
-        data_x.append(utterance)
-        data_y.append(intent['tag'])
-
-    if intent['tag'] not in classes:
-        classes.append(intent['tag'])
-
-lemmatizer = WordNetLemmatizer()
-
-words = [lemmatizer.lemmatize(word.lower()) for word in words if word not in string.punctuation]
-
-words = sorted(set(words))
-classes = sorted(set(classes))
-
-training = []
-out_empty = [0]* len(classes)
-
-for idx, doc in enumerate(data_x):
-    bow = []
-    text = lemmatizer.lemmatize(doc.lower())
-
-    for word in words:
-        bow.append(1) if word in text else bow.append(0)
-
-    output_row = list(out_empty)
-    output_row[classes.index(data_y[idx])] = 1
-
-    training.append([bow, output_row])
-
-random.shuffle(training)
-training = np.array(training, dtype=object)
-
-train_x = np.array(list(training[:,0]))
-train_y = np.array(list(training[:,1]))
-
-input_shape = (len(train_x[0]), )
-output_shape = len(train_y[0])
-
-model = Sequential()
-model.add(Dense(128, input_shape=input_shape, activation="relu"))
-model.add(Dropout(0.5))
-model.add(Dense(64, activation="relu"))
-model.add(Dropout(0.3))
-model.add(Dense(output_shape, activation="softmax"))
-adam = tf.keras.optimizers.Adam(learning_rate=0.01, decay=1e-6)
-model.compile(loss='categorical_crossentropy',
-    optimizer=adam,
-    metrics=["accuracy"])
-
-model.fit(x=train_x, y=train_y, epochs=500, verbose=0)
-
-def clean_text(text):
-    tokens = nltk.word_tokenize(text)
-    tokens = [lemmatizer.lemmatize(word.lower()) for word in tokens]
-    return tokens
-
-def bag_of_words(command, words):
-    tokens = clean_text(command)
-    bow = [0]* len(words)
-
-    for token in tokens:
-        for idx, word in enumerate(words):
-            if word == token:
-                bow[idx] = 1
-
-    return np.array(bow)
-
-def get_intent(command, words, classes):
-    bow = bag_of_words(command, words)
-    result = model.predict(np.array([bow]))[0]
-    thresh = 0.5
-
-    y_pred = [[idx, res] for idx, res in enumerate(result) if res > thresh]
-    y_pred.sort(key=lambda x: x[1], reverse=True)
-
-    return_list =[]
-
-    for pred in y_pred:
-        return_list.append(classes[pred[0]])
-    return return_list
-
-def get_response(intents, data):
-    tag = intents[0]
-    list_of_intents = data['intents']
-    for intent in list_of_intents:
-        if intent['tag'] == tag:
-            response = random.choice(intent['response'])
-            break
-    return response
-
-while True:
-    command = input("Enter your command: ")
-    intents = get_intent(command, words, classes)
-    response = get_response(intents, data)
-    print('JARVIS: ',response)
+    while True:
+        command = input("Enter your command: ")
+        intents = training_model.get_intent(trained_model, command)
+        response = TrainingModel.get_response(intents, model.data)
+        print(intents, ' : ', response)
