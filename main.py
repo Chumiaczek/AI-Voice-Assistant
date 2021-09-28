5ea9feaa11b53acc879983e966ff07f2c0dc9664
Grzegorz <prywatne.grzegorztabat@gmail.com>
2021-09-28 20:45:38 +0200

    Update

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import model
 from model.model_training import TrainingModel
 import speech_recognition as sr
 
-def read_voice_cmd(self):
+def read_voice_cmd(speech):
     voice_input = ''
     try:
         with sr.Microphone() as source:
             print("Listening...")
-            audio = self.speech.listen(source=source, timeout=5, phrase_time_limit=5)
-        voice_input = self.speech.recognize_google(audio)
-        self.logger.info('Input: {}'.format(voice_input))
+            audio = speech.listen(source=source, timeout=5, phrase_time_limit=5)
+        voice_input = speech.recognize_google(audio)
+        print('Input: {}'.format(voice_input))
     except sr.UnknownValueError:
         pass
     except sr.RequestError:
@@ -28,8 +28,10 @@ if __name__ == '__main__':
     training_model = TrainingModel(words, classes, model.data_x, model.data_y)
     trained_model = training_model.train()
 
+    speech = sr.Recognizer()
+
     while True:
-        command = input("Enter your command: ")
+        command = read_voice_cmd(speech)
         intents = training_model.get_intent(trained_model, command)
         response = TrainingModel.get_response(intents, model.data)
         print(intents, ' : ', response)
